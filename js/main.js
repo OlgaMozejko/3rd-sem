10fed94fd057a6c0fac58b77f168f4df61a18bf7
OlgaMozejko <78202579+OlgaMozejko@users.noreply.github.com>
2021-12-01 10:41:00 +0100

    om
    snap scrolling

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 "use strict";
 
+var scrollPosition = window.scrollY;
+var cover = document.getElementsByClassName("cover")[0];
+
+window.addEventListener("scroll", function () {
+    scrollPosition = window.scrollY;
+
+    if (scrollPosition >= 1) {
+        cover.classList.add("cover-close");
+    } else {
+        cover.classList.remove("cover-close");
+    }
+});
