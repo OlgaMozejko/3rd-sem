be4cce56b0fbab18698bb46a19d82d7fae9256e5
Marius <79192265+mariusbarbutsa@users.noreply.github.com>
2021-12-04 20:03:53 +0100

    classes

---
+++ js/firebase-service.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5Km7PRa3jrZRSTolSuaKxOeMcEUt9ius",
+
+  authDomain: "den-gule-cafe.firebaseapp.com",
+
+  projectId: "den-gule-cafe",
+
+  storageBucket: "den-gule-cafe.appspot.com",
+
+  messagingSenderId: "365916633510",
+
+  appId: "1:365916633510:web:fbb4b9874d16463d563767",
+
+  measurementId: "G-3P1NE0VQSM"
+};
+firebase.initializeApp(firebaseConfig);
+
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const firebaseDB = firebase.firestore();
--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 "use strict";
 
+import SpaService from "./spa-service.js";
+import MenuService from "./menu-service.js";
+
+let _spaService = new SpaService("home");
+let _menuService = new MenuService();
+let _selectedUserId = "";
+let _selectedImgFile = "";
+
+window.pageChange = function () {
+    _spaService.pageChange();
+}
+
 var scrollPosition = window.scrollY;
 var cover = document.getElementsByClassName("cover")[0];
 
