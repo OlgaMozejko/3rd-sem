4dedd4e3fecde3e3e8ea9af237127bab2bbbb3e4
vladaafanasjeva <73819965+vladaafanasjeva@users.noreply.github.com>
2021-12-06 10:42:35 +0100

    disappoinment

--- js/menu-service.js
+++ js/menu-service.js
@@ -1,11 +1,9 @@
-import {
-  firebaseDB
-} from "./firebase-service.js";
+import { firebaseDB } from "./firebase-service.js";
 
 import {
-	collection,
-	onSnapshot,
-	doc,
+  collection,
+  onSnapshot,
+  doc,
 } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
 
 export default class MenuService {
@@ -15,38 +13,32 @@ export default class MenuService {
   }
 
   read() {
-		// ========== READ ==========
-		// watch the database ref for changes
-		onSnapshot(this.menusRef, snapshot => {
-			// mapping snapshot data from firebase in to user objects
-			this.menus = snapshot.docs.map(doc => {
-				const menu = doc.data();
-				menu.id = doc.id;
-				return menu;
-			});
+    // ========== READ ==========
+    // watch the database ref for changes
+    onSnapshot(this.menusRef, (snapshot) => {
+      // mapping snapshot data from firebase in to user objects
+      this.menus = snapshot.docs.map((doc) => {
+        const menu = doc.data();
+        menu.id = doc.id;
+        return menu;
+      });
 
       console.log(this.menus);
-		//	this.appendUsers(this.menus);
-		});
-	}
+      this.appendFoods(this.menus);
+    });
+  }
 
-  // // append users to the DOM
-  // appendUsers(users) {
-  //   let htmlTemplate = "";
-  //   for (let user of users) {
-  //     htmlTemplate += `
-  //     <article>
-  //       <h2>${user.name}</h2>
-  //       <img src="${user.img || 'img/placeholder.jpg'}">
-  //       <p><a href="mailto:${user.mail}">${user.mail}</a></p>
-  //       <button onclick="selectUser('${user.id}','${user.name}', '${user.mail}', '${user.img}')">Update</button>
-  //       <button onclick="deleteUser('${user.id}')">Delete</button>
-  //     </article>
-  //     `;
-  //   }
-  //   document.querySelector('#user-container').innerHTML = htmlTemplate;
-  // }
-
-
-
+  // append users to the DOM
+  appendFoods(items) {
+    let htmlTemplate = "";
+    for (let item of items) {
+      htmlTemplate += `
+      <article>
+        <h2>${item.food.foods.id}</h2>
+      </article>
+      `;
+    }
+    console.log("yes");
+    document.querySelector("#menu-list").innerHTML = htmlTemplate;
+  }
 }
